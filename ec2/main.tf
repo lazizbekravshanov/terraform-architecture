b90109e2d288bacc64c83d55caf090d137b8a30f
Lazizbek <lazizravshanov1998@gmail.com>
2023-11-25 15:40:13 -0600

    added ec2

---
+++ ec2/main.tf
@@ -0,0 +1,48 @@
+# pulls info from another workspace
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "lazizbekravshanov"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+# pulls info from rds workspace
+data "terraform_remote_state" "rds" {
+  backend = "remote"
+
+  config = {
+    organization = "lazizbekravshanov"
+    workspaces = {
+      name = "rds"
+    }
+  }
+}
+
+# Pulls AMI information
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+# Creates EC2 instance
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id     = data.terraform_remote_state.vpc.outputs.public_subnets[0]
+}
