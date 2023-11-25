e4235e6ebc779571f4a24cd1e68b07bcc8dbd049
Lazizbek <lazizravshanov1998@gmail.com>
2023-11-25 15:26:42 -0600

    added subnets

---
+++ rds/main.tf
@@ -0,0 +1,19 @@
+# pulls info from another workspace
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "lazizbekravshanov"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+# Creates subnet group with private subnets
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [
+    data.terraform_remote_state.vpc.outputs.private_subnets
+  ]
+}
